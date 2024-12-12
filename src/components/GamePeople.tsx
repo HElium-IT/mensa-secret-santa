cc8ccfd0e2d381126d517a4aaf4c960db73c8e48
HElium-IT <elio.palomba.dev@gmail.com>
2024-12-12 23:14:58 +0100

    fix: remove authMode from update calls in GamePeople and GamePhaseUpdater components

--- src/components/GamePeople.tsx
+++ src/components/GamePeople.tsx
@@ -33,7 +33,7 @@ function GamePeople({ gamePeople, filterRole, userRole }: {
         const { errors } = await client.models.GamePerson.update({
             id: gamePerson.id,
             role: 'ADMIN',
-        }, { authMode: 'userPool' })
+        })
         if (errors) {
             alert(errors);
         }
