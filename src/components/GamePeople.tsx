15e213f4865ad47f0d8ea2e724875a74398147cc
HElium-IT <elio.palomba.dev@gmail.com>
2024-12-17 01:55:40 +0100

    feat: enhance GamePeople component with sorting and gift tracking logic

--- src/components/GamePeople.tsx
+++ src/components/GamePeople.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useMemo } from "react";
 import type { Schema } from "../../amplify/data/resource";
-import { gamePersonRoleToIcon } from '../utils';
+import { gamePersonRoleToIcon, sortGamePeople } from '../utils';
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
@@ -11,10 +11,12 @@ function GamePeople({ gamePeople, filterRole, userRole }: {
     readonly userRole: Schema["GamePerson"]["type"]["role"],
 }) {
     const [hasGift, setHasGift] = useState<Record<string, boolean>>({});
+    const [wonGift, setWonGift] = useState<Record<string, boolean>>({});
+
     const gamePeopleMemo = useMemo(
         () => gamePeople
             .filter(gamePerson => gamePerson.role === filterRole)
-            .sort((a, b) => a.personId.localeCompare(b.personId)),
+            .sort(sortGamePeople),
         [gamePeople]
     );
 
@@ -28,11 +30,20 @@ function GamePeople({ gamePeople, filterRole, userRole }: {
         }).subscribe({
             next: ({ items: gifts }) => {
                 const giftOwners = gifts.map(gift => gift.ownerPersonId);
+
                 const hasGift = gamePeopleMemo.reduce((acc, gamePerson) => {
                     acc[gamePerson.personId] = giftOwners.includes(gamePerson.personId);
                     return acc;
                 }, {} as Record<string, boolean>);
                 setHasGift(hasGift);
+
+                const wonGift = gamePeopleMemo.reduce((acc, gamePerson) => {
+                    acc[gamePerson.personId] = gifts.some(gift => gift.winnerPersonId === gamePerson.personId);
+                    return acc;
+                }, {} as Record<string, boolean>);
+                setWonGift(wonGift);
+
+                gamePeopleMemo.sort((a, b) => sortGamePeople(a, b, hasGift, wonGift));
             }
         });
 
@@ -75,6 +86,7 @@ function GamePeople({ gamePeople, filterRole, userRole }: {
                     <li key={gamePerson.personId}>
                         {(userRole === "CREATOR" || userRole === "ADMIN") && !gamePerson.acceptedInvitation && '📧'}
                         {hasGift[gamePerson.personId] && '🎁'}
+                        {wonGift[gamePerson.personId] && '🏆'}
                         {gamePerson.personId}
                         {userRole === "CREATOR" && gamePerson.role === 'PLAYER' &&
                             <button style={{ padding: 1 }} onClick={() => upgradeToAdmin(gamePerson)}>
